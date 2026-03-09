2a19162ae5df30725ef5d14f59d152696aaa7f3c
thomasjvalenzuela <thomasjvalenzuela@gmail.com>
2026-03-09 16:18:41 -0500

    Update Vite config to set base path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: parseInt(process.env.PORT) || 5174,
-  },
+  base: '/fiscus-Ai-finance/',
 })
